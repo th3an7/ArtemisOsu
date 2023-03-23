de29ffb70aaf69761de4f3c3fe79dcde8af7abe7
th3an7 <lukaszordon21@gmail.com>
2023-03-23 21:36:54 +0100

    Create simple DataModel

--- ArtemisOsu/DataModels/OsuDataModel.cs
+++ ArtemisOsu/DataModels/OsuDataModel.cs
@@ -1,8 +1,50 @@
 ﻿using Artemis.Core.Modules;
+using Artemis.Core.Properties;
 
 namespace ArtemisOsu.DataModels
 {
     public class OsuDataModel : DataModel
     {
+        public OsuPlayerDataModel Player { get; set; } = new OsuPlayerDataModel();
+        public OsuBeatmapDataModel Beatmap { get; set; } = new OsuBeatmapDataModel();
+        public OsuGameDataModel Game { get; set; } = new OsuGameDataModel();
+    }
+
+    public class OsuPlayerDataModel : DataModel
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int Combo { get; set; }
+        public int MaxCombo { get; set; }
+        public double HP { get; set; }
+        public int H300 { get; set; }
+        public int H100 { get; set; }
+        public int H50 { get; set; }
+        public int Katu { get; set; }
+        public int Geki { get; set; }
+        public int Miss { get; set; }
+
+
+    }
+
+    public class OsuBeatmapDataModel : DataModel
+    {
+        public string Name { get; set; }
+        public float AR { get; set; }
+        public float CS { get; set; }
+        public float HP { get; set; }
+        public float OD { get; set; }
+        public int Id { get; set; }
+        public int SetId { get; set; }
+        public short Status { get; set; }
+    }
+
+    public class OsuGameDataModel : DataModel
+    {
+        public int Gamemode { get; set; }
+        public int Mods { get; set; }
+        public string Status { get; set; }
+        public int RawStatus { get; set; }
+        public bool IsChatExpanded { get; set; }
     }
 }
--- ArtemisOsu/OsuModule.cs
+++ ArtemisOsu/OsuModule.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Serilog;
 using OsuMemoryDataProvider;
 using OsuMemoryDataProvider.OsuMemoryModels;
+using OsuMemoryDataProvider.OsuMemoryModels.Direct;
 
 namespace ArtemisOsu
 {
@@ -73,6 +74,33 @@ namespace ArtemisOsu
             {
                 _baseAddresses.LeaderBoard.Players.Clear();
             }
+
+            DataModel.Beatmap.Name = _baseAddresses.Beatmap.MapString;
+            DataModel.Beatmap.AR = _baseAddresses.Beatmap.Ar;
+            DataModel.Beatmap.CS = _baseAddresses.Beatmap.Cs;
+            DataModel.Beatmap.HP = _baseAddresses.Beatmap.Hp;
+            DataModel.Beatmap.OD = _baseAddresses.Beatmap.Od;
+            DataModel.Beatmap.Id = _baseAddresses.Beatmap.Id;
+            DataModel.Beatmap.SetId = _baseAddresses.Beatmap.SetId;
+            DataModel.Beatmap.Status = _baseAddresses.Beatmap.Status;
+
+            DataModel.Player.Name = _baseAddresses.Player.Username;
+            DataModel.Player.Score = _baseAddresses.Player.Score;
+            DataModel.Player.Combo = _baseAddresses.Player.Combo;
+            DataModel.Player.MaxCombo = _baseAddresses.Player.MaxCombo;
+            DataModel.Player.HP = _baseAddresses.Player.HP;
+            DataModel.Player.H300 = _baseAddresses.Player.Hit300;
+            DataModel.Player.H100 = _baseAddresses.Player.Hit100;
+            DataModel.Player.H50 = _baseAddresses.Player.Hit50;
+            DataModel.Player.Katu = _baseAddresses.Player.HitKatu;
+            DataModel.Player.Geki = _baseAddresses.Player.HitGeki;
+            DataModel.Player.Miss = _baseAddresses.Player.HitMiss;
+
+            DataModel.Game.Gamemode = _baseAddresses.GeneralData.GameMode;
+            DataModel.Game.Mods = _baseAddresses.GeneralData.Mods;
+            DataModel.Game.RawStatus = _baseAddresses.GeneralData.RawStatus;
+            DataModel.Game.Status = _baseAddresses.GeneralData.OsuStatus.ToString();
+            DataModel.Game.IsChatExpanded = _baseAddresses.GeneralData.ChatIsExpanded;
         }
     }
 }
